a46e0dba29ab6f4f48fb4fc28d27c010e0128bac
Daniel Jilg <daniel.jilg@fineway.de>
2017-10-02 12:37:33 +0200

    Dots instead of lines

--- code/mpl_test.py
+++ code/mpl_test.py
@@ -2,10 +2,15 @@ from random import randrange
 
 import matplotlib.pyplot as plt
 
+# TODO: Check out blitting
+# https://stackoverflow.com/a/15724978/54547
+
 plt.xkcd()
 plt.ion()
 for _ in range(20):
-    plt.plot([randrange(-200, 200), randrange(-200, 200)])
+    plt.plot([randrange(-200, 200), randrange(-200, 200)],
+             [randrange(-200, 200), randrange(-200, 200)],
+             'o')
     plt.pause(0.0001)
 
 # Show results for a second before exiting
