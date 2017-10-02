88ea6553f84f6916e303357ed6c0064ff71d1f47
Daniel Jilg <daniel.jilg@fineway.de>
2017-10-02 13:20:14 +0200

    Multiple Dots

--- code/mpl_test.py
+++ code/mpl_test.py
@@ -13,21 +13,22 @@ plt.ylim(-200, 200)
 
 # In case we dont want to show axis dots
 # plt.axis('off')
-dots = plt.plot(0, 0, 'o', markersize=13.0)
+dots = [plt.plot(0, 0, 'o', markersize=3.5 * i) for i in range(10)]
 
 for _ in range(200):
     # Update The Dot
-    dot = dots[0]
-    old_ydata = dot.get_ydata()
-    old_xdata = dot.get_xdata()
-    ydata = [value + randrange(-9, 10) for value in old_ydata]
-    xdata = [value + randrange(-9, 10) for value in old_xdata]
-    dot.set_ydata(ydata)
-    dot.set_xdata(xdata)
+    for dotdata in dots:
+        dot = dotdata[0]
+        old_ydata = dot.get_ydata()
+        old_xdata = dot.get_xdata()
+        ydata = [value + randrange(-9, 10) for value in old_ydata]
+        xdata = [value + randrange(-9, 10) for value in old_xdata]
+        dot.set_ydata(ydata)
+        dot.set_xdata(xdata)
 
-    # Update the trailing line
-    plt.plot([old_xdata, xdata], [old_ydata, ydata], dot.get_color())
-    plt.pause(0.0101)
+        # Update the trailing line
+        plt.plot([old_xdata, xdata], [old_ydata, ydata], dot.get_color())
+    plt.pause(0.0001)
 
 # Pause without quitting to keep showing the graph, but listen
 # to KeyboardInterrupt signals to quit on Ctrl-C
