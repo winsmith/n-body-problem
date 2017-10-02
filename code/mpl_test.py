dfb1eb071fb6dd58347c2132d058ef1fd27281b6
Daniel Jilg <daniel.jilg@fineway.de>
2017-10-02 12:29:14 +0200

    Mat Plot Interactive Test with lines

--- code/mpl_test.py
+++ code/mpl_test.py
@@ -1,4 +1,16 @@
+from random import randrange
+
 import matplotlib.pyplot as plt
+
 plt.xkcd()
 plt.ion()
-plt.plot([1.6, 2.7])
+for _ in range(20):
+    plt.plot([randrange(-200, 200), randrange(-200, 200)])
+    plt.pause(0.0001)
+
+# Show results for a second before exiting
+plt.pause(1.0)
+
+# uncomment to keep on screen
+# plt.ioff()
+# plt.show()
