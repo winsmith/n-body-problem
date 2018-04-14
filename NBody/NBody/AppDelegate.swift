d74f6cdbaea93ce827a5b364d19d84fec7a7df90
Daniel Jilg <winsmith@winsmith.de>
2018-04-14 10:06:29 +0200

    Add Xcode Project

---
+++ NBody/NBody/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NBody
+//
+//  Created by Daniel Jilg on 14.04.18.
+//  Copyright © 2018 breakthesystem. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NBody/NBody/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NBody
+//
+//  Created by Daniel Jilg on 14.04.18.
+//  Copyright © 2018 breakthesystem. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
